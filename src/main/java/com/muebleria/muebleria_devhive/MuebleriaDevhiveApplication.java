a42c1d8a03661fadab165cf22dac6137b9bfbce1
sdelacruz <sdelacruz.mca@gmail.com>
2024-07-14 21:35:31 -0600

    Avances en contenerizacion de back y clases para kubernates.

--- src/main/java/com/muebleria/muebleria_devhive/MuebleriaDevhiveApplication.java
+++ src/main/java/com/muebleria/muebleria_devhive/MuebleriaDevhiveApplication.java
@@ -3,12 +3,10 @@ package com.muebleria.muebleria_devhive;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MuebleriaDevhiveApplication {
 
 	@Bean
---
+++ src/main/java/com/muebleria/muebleria_devhive/config/ConfigMapProperties.java
@@ -0,0 +1,8 @@
+package com.muebleria.muebleria_devhive.config;
+
+//@Configuration
+//@ConfigurationProperties(prefix = "configmap")
+public class ConfigMapProperties {
+	
+	private String databaseUri = "http://localhost:8080/inmuebles/";
+}
